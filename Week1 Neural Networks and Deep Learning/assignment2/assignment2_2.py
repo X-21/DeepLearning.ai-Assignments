1c6c3a921a208b0a975b2731cbd83ce580d74c5c
edison <Edison-404@outlook.com>
2019-01-11 18:00:17 +0800

    finish 2_2

--- Week1 Neural Networks and Deep Learning/assignment2/assignment2_2.py
+++ Week1 Neural Networks and Deep Learning/assignment2/assignment2_2.py
@@ -1,32 +1,134 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import h5py
-import scipy
-from PIL import Image
 from scipy import ndimage
 from lr_utils import load_dataset
+from scipy import misc
+
+
+def sigmoid(z):
+    s = 1 / (1 + (np.exp(-z)))
+    return s
+
+
+def initialize_with_zeros(dim):
+    w = np.zeros((dim, 1))
+    b = 0
+
+    assert (w.shape == (dim, 1))
+    assert (isinstance(b, float) or isinstance(b, int))
+
+    return w, b
+
+
+def propagate(w, b, X, Y):
+    m = X.shape[1]
+
+    # FORWARD PROPAGATION (FROM X TO COST)
+    A = sigmoid(np.dot(w.T, X) + b)
+    cost = (1 / m) * (np.dot(np.log(A), (-Y).T) - np.dot(np.log(1 - A), (1 - Y).T))
+
+    # BACKWARD PROPAGATION
+    dw = (1 / m) * np.dot(X, (A - Y).T)
+    db = (1 / m) * np.sum(A - Y)
+    assert (dw.shape == w.shape)
+    assert (db.dtype == float)
+    cost = np.squeeze(cost)
+    assert (cost.shape == ())
+
+    grads = {"dw": dw,
+             "db": db}
+
+    return grads, cost
+
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost=False):
+    costs = []
+
+    dw, db = 0, 0
+    for i in range(num_iterations):
+
+        grads, cost = propagate(w, b, X, Y)
+        dw = grads["dw"]
+        db = grads["db"]
+
+        w = w - learning_rate * dw
+        b = b - learning_rate * db
+
+        # Record the costs
+        if i % 100 == 0:
+            costs.append(cost)
+
+        # Print the cost every 100 training examples
+        if print_cost and i % 100 == 0:
+            print("Cost after iteration %i: %f" % (i, float(cost)))
+
+    params = {"w": w,
+              "b": b}
+
+    grads = {"dw": dw,
+             "db": db}
+
+    return params, grads, costs
+
+
+def predict(w, b, X):
+    m = X.shape[1]
+    Y_prediction = np.zeros((1, m))
+    w = w.reshape(X.shape[0], 1)
+
+    # Compute vector "A" predicting the probabilities of a cat being present in the picture
+    A = sigmoid(np.dot(w.T, X) + b)
+
+    for i in range(A.shape[1]):
+        # Convert probabilities A[0,i] to actual predictions p[0,i]
+        Y_prediction[0, i] = 1 if A[0, i] > 0.5 else 0
+
+    assert (Y_prediction.shape == (1, m))
+    return Y_prediction
+
+
+def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0.5, print_cost=False):
+    # initialize parameters with zeros (≈ 1 line of code)
+    w, b = initialize_with_zeros(X_train.shape[0])
+
+    # Gradient descent (≈ 1 line of code)
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
+
+    # Retrieve parameters w and b from dictionary "parameters"
+    w = parameters["w"]
+    b = parameters["b"]
+
+    # Predict test/train set examples (≈ 2 lines of code)
+    Y_prediction_test = predict(w, b, X_test)
+    Y_prediction_train = predict(w, b, X_train)
+
+    # Print train/test Errors
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+
+    d = {"costs": costs,
+         "Y_prediction_test": Y_prediction_test,
+         "Y_prediction_train": Y_prediction_train,
+         "w": w,
+         "b": b,
+         "learning_rate": learning_rate,
+         "num_iterations": num_iterations}
+
+    return d
+
 
 if __name__ == '__main__':
     train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
     index = 5
-    plt.ion()
+
     plt.imshow(train_set_x_orig[index])
-    # plt.show()
+    plt.show()
     print("y = " + str(train_set_y[:, index]) + ", it's a '"
           + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") + "' picture.")
-    '''
-    **Exercise:** Find the values for:
-    - m_train (number of training examples)
-    - m_test (number of test examples)
-    - num_px (= height = width of a training image)
-    Remember that `train_set_x_orig` is a numpy-array of shape (m_train, num_px, num_px, 3). 
-    For instance, you can access `m_train` by writing `train_set_x_orig.shape[0]`.
-    '''
-    # START CODE HERE # (≈ 3 lines of code)
+
     m_train = train_set_x_orig.shape[0]
     m_test = test_set_x_orig.shape[0]
     num_px = train_set_x_orig.shape[1]
-    # END CODE HERE #
 
     print("Number of training examples: m_train = " + str(m_train))
     print("Number of testing examples: m_test = " + str(m_test))
@@ -37,26 +139,8 @@ if __name__ == '__main__':
     print("test_set_x shape: " + str(test_set_x_orig.shape))
     print("test_set_y shape: " + str(test_set_y.shape))
 
-    '''
-    For convenience, you should now reshape images of shape (num_px, num_px, 3) 
-    in a numpy-array of shape (num_px  ∗  num_px  ∗  3, 1). 
-    After this, our training (and test) dataset is a numpy-array 
-    where each column represents a flattened image. 
-    There should be m_train (respectively m_test) columns.
-    Exercise: Reshape the training and test data sets so that images of size (num_px, num_px, 3) are 
-    flattened into single vectors of shape (num_px  ∗  num_px  ∗  3, 1).
-
-    A trick when you want to flatten a matrix X of shape (a,b,c,d) 
-    to a matrix X_flatten of shape (b ∗ c ∗ d, a) is to use:
-
-    X_flatten = X.reshape(X.shape[0], -1).T      # X.T is the transpose of X
-    '''
-    # Reshape the training and test examples
-
-    # START CODE HERE # (≈ 2 lines of code)
     train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
     test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
-    # END CODE HERE #
 
     print("train_set_x_flatten shape: " + str(train_set_x_flatten.shape))
     print("train_set_y shape: " + str(train_set_y.shape))
@@ -64,4 +148,88 @@ if __name__ == '__main__':
     print("test_set_y shape: " + str(test_set_y.shape))
     print("sanity check after reshaping: " + str(train_set_x_flatten[0:5, 0]))
 
-a = 1
+    train_set_x = train_set_x_flatten / 255.
+    test_set_x = test_set_x_flatten / 255.
+
+    print("sigmoid([0, 2]) = " + str(sigmoid(np.array([0, 2]))))
+
+    # Initializing parameters
+    dim = 2
+    w, b = initialize_with_zeros(dim)
+    print("w = " + str(w))
+    print("b = " + str(b))
+
+    # Forward and Backward propagation
+
+    w, b, X, Y = np.array([[1], [2]]), 2, np.array([[1, 2], [3, 4]]), np.array([[1, 0]])
+    grads, cost = propagate(w, b, X, Y)
+    print("dw = " + str(grads["dw"]))
+    print("db = " + str(grads["db"]))
+    print("cost = " + str(cost))
+
+    # Optimization
+    params, grads, costs = optimize(w, b, X, Y, num_iterations=100,
+                                    learning_rate=0.009, print_cost=False)
+
+    print("w = " + str(params["w"]))
+    print("b = " + str(params["b"]))
+    print("dw = " + str(grads["dw"]))
+    print("db = " + str(grads["db"]))
+    print(costs)
+
+    # predict
+    print("predictions = " + str(predict(w, b, X)))
+
+    # Merge all functions into a model
+    d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations=2000, learning_rate=0.005,
+              print_cost=True)
+    index = 1
+    plt.figure()
+    plt.imshow(test_set_x[:, index].reshape((num_px, num_px, 3)))
+    print("y = " + str(test_set_y[0, index]) + ", you predicted that it is a \"" + classes[
+        int(d["Y_prediction_test"][0, index])].decode("utf-8") + "\" picture.")
+    plt.show()
+
+    # Plot learning curve (with costs)
+    costs = np.squeeze(d['costs'])
+    plt.figure()
+    plt.plot(costs)
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per hundreds)')
+    plt.title("Learning rate =" + str(d["learning_rate"]))
+    plt.show()
+
+    # Further analysis (optional/ungraded exercise)
+    learning_rates = [0.01, 0.001, 0.0001]
+    models = {}
+    plt.figure()
+    for i in learning_rates:
+        print("learning rate is: " + str(i))
+        models[str(i)] = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations=1500, learning_rate=i,
+                               print_cost=False)
+        print('\n' + "-------------------------------------------------------" + '\n')
+
+    for i in learning_rates:
+        plt.plot(np.squeeze(models[str(i)]["costs"]), label=str(models[str(i)]["learning_rate"]))
+
+    plt.ylabel('cost')
+    plt.xlabel('iterations')
+
+    legend = plt.legend(loc='upper center', shadow=True)
+    frame = legend.get_frame()
+    frame.set_facecolor('0.90')
+    plt.show()
+
+    plt.figure()
+    # Test with your own image (optional/ungraded exercise)
+    my_image = "cat_in_iran.jpg"  # change this to the name of your image file
+    # We preprocess the image to fit your algorithm.
+    fname = "images/" + my_image
+    image = np.array(ndimage.imread(fname, flatten=False))
+    my_image = misc.imresize(image, size=(num_px, num_px)).reshape((1, num_px * num_px * 3)).T
+    my_predicted_image = predict(d["w"], d["b"], my_image)
+
+    plt.imshow(image)
+    print("y = " + str(np.squeeze(my_predicted_image)) + ", your algorithm predicts a \"" + classes[
+        int(np.squeeze(my_predicted_image)),].decode("utf-8") + "\" picture.")
+    plt.show()
