8d26e5757b44e5e84e25d99457c4f72010d4eba7
edison <Edison-404@outlook.com>
2019-01-18 10:18:30 +0800

    finish 1— course2 week1 1_2

--- 2.Improving Deep Neural Networks/Week1 Practical aspects of Deep Learning/1_2_Regularization.py
+++ 2.Improving Deep Neural Networks/Week1 Practical aspects of Deep Learning/1_2_Regularization.py
@@ -3,9 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from reg_utils import sigmoid, relu, plot_decision_boundary, initialize_parameters, load_2D_dataset, predict_dec
 from reg_utils import compute_cost, predict, forward_propagation, backward_propagation, update_parameters
-import sklearn
-import sklearn.datasets
-import scipy.io
 
 
 def model(X, Y, learning_rate=0.3, num_iterations=30000, print_cost=True, lambd=0.0, keep_prob=1):
@@ -250,8 +247,6 @@ def backward_propagation_with_dropout(X, Y, cache, keep_prob):
     return gradients
 
 
-
-
 if __name__ == '__main__':
     plt.rcParams['figure.figsize'] = (7.0, 4.0)  # set default size of plots
     plt.rcParams['image.interpolation'] = 'nearest'
@@ -263,43 +258,43 @@ if __name__ == '__main__':
     plt.close()
 
     # Non-regularized model
-    # parameters = model(train_X, train_Y)
-    # print("On the training set:")
-    # predictions_train = predict(train_X, train_Y, parameters)
-    # print("On the test set:")
-    # predictions_test = predict(test_X, test_Y, parameters)
-    #
-    # plt.figure()
-    # plt.title("Model without regularization")
-    # axes = plt.gca()
-    # axes.set_xlim([-0.75, 0.40])
-    # axes.set_ylim([-0.75, 0.65])
-    # plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
-    # plt.close()
+    parameters = model(train_X, train_Y)
+    print("On the training set:")
+    predictions_train = predict(train_X, train_Y, parameters)
+    print("On the test set:")
+    predictions_test = predict(test_X, test_Y, parameters)
+
+    plt.figure()
+    plt.title("Model without regularization")
+    axes = plt.gca()
+    axes.set_xlim([-0.75, 0.40])
+    axes.set_ylim([-0.75, 0.65])
+    plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+    plt.close()
 
     # L2 Regularization
-    # A3, Y_assess, parameters = compute_cost_with_regularization_test_case()
-    # print("cost = " + str(compute_cost_with_regularization(A3, Y_assess, parameters, lambd=0.1)))
-    #
-    # X_assess, Y_assess, cache = backward_propagation_with_regularization_test_case()
-    # grads = backward_propagation_with_regularization(X_assess, Y_assess, cache, lambd=0.7)
-    # print("dW1 = " + str(grads["dW1"]))
-    # print("dW2 = " + str(grads["dW2"]))
-    # print("dW3 = " + str(grads["dW3"]))
-    #
-    # parameters = model(train_X, train_Y, lambd=0.7)
-    # print("On the train set:")
-    # predictions_train = predict(train_X, train_Y, parameters)
-    # print("On the test set:")
-    # predictions_test = predict(test_X, test_Y, parameters)
-    #
-    # plt.figure()
-    # plt.title("Model with L2-regularization")
-    # axes = plt.gca()
-    # axes.set_xlim([-0.75, 0.40])
-    # axes.set_ylim([-0.75, 0.65])
-    # plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
-    # plt.close()
+    A3, Y_assess, parameters = compute_cost_with_regularization_test_case()
+    print("cost = " + str(compute_cost_with_regularization(A3, Y_assess, parameters, lambd=0.1)))
+
+    X_assess, Y_assess, cache = backward_propagation_with_regularization_test_case()
+    grads = backward_propagation_with_regularization(X_assess, Y_assess, cache, lambd=0.7)
+    print("dW1 = " + str(grads["dW1"]))
+    print("dW2 = " + str(grads["dW2"]))
+    print("dW3 = " + str(grads["dW3"]))
+
+    parameters = model(train_X, train_Y, lambd=0.7)
+    print("On the train set:")
+    predictions_train = predict(train_X, train_Y, parameters)
+    print("On the test set:")
+    predictions_test = predict(test_X, test_Y, parameters)
+
+    plt.figure()
+    plt.title("Model with L2-regularization")
+    axes = plt.gca()
+    axes.set_xlim([-0.75, 0.40])
+    axes.set_ylim([-0.75, 0.65])
+    plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+    plt.close()
 
     # Dropout
     # Forward propagation with dropout for 3 layer neural network
@@ -307,12 +302,11 @@ if __name__ == '__main__':
     A3, cache = forward_propagation_with_dropout(X_assess, parameters, keep_prob=0.7)
     print("A3 = " + str(A3))
 
-    #Backward propagation with dropout
+    # Backward propagation with dropout
     X_assess, Y_assess, cache = backward_propagation_with_dropout_test_case()
     gradients = backward_propagation_with_dropout(X_assess, Y_assess, cache, keep_prob=0.8)
     print("dA1 = " + str(gradients["dA1"]))
     print("dA2 = " + str(gradients["dA2"]))
-
 
     parameters = model(train_X, train_Y, keep_prob=0.86, learning_rate=0.3)
     print("On the train set:")
@@ -320,5 +314,10 @@ if __name__ == '__main__':
     print("On the test set:")
     predictions_test = predict(test_X, test_Y, parameters)
 
-
-    a = 1
+    plt.figure()
+    plt.title("Model with dropout")
+    axes = plt.gca()
+    axes.set_xlim([-0.75, 0.40])
+    axes.set_ylim([-0.75, 0.65])
+    plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+    plt.close()
