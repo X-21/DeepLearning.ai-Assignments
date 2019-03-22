74b9ae3a4a89bebfddfef8f780021f62f577fe69
edison <Edison-404@outlook.com>
2019-03-22 17:14:08 +0800

    finish c4 week1 2_1

--- 4. Convolutional Neural Networks/Week1 Foundations of Convolutional Neural Networks/1_2_Convolutional Neural Networks_Application.py
+++ 4. Convolutional Neural Networks/Week1 Foundations of Convolutional Neural Networks/1_2_Convolutional Neural Networks_Application.py
@@ -74,7 +74,7 @@ def forward_propagation(X, parameters):
     # RELU
     A1 = tf.nn.relu(Z1)
     # MAXPOOL: window 8x8, sride 8, padding 'SAME'
-    P1 = tf.nn.max_pool(A1, ksize=[1, 8, 8, 1], strides=[1, 8, 8, 1], padding='SAME')
+    P1 = tf.nn.max_pool(A1, ksize=[1, 4, 4, 1], strides=[1, 4, 4, 1], padding='SAME')
     # CONV2D: filters W2, stride 1, padding 'SAME'
     Z2 = tf.nn.conv2d(P1, W2, strides=[1, 1, 1, 1], padding='SAME')
     # RELU
@@ -90,6 +90,121 @@ def forward_propagation(X, parameters):
     return Z3
 
 
+def compute_cost(Z3, Y):
+    """
+    Computes the cost
+
+    Arguments:
+    Z3 -- output of forward propagation (output of the last LINEAR unit), of shape (6, number of examples)
+    Y -- "true" labels vector placeholder, same shape as Z3
+
+    Returns:
+    cost - Tensor of the cost function
+    """
+
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=Z3, labels=Y))
+
+    return cost
+
+
+def model(X_train, Y_train, X_test, Y_test, learning_rate=0.009,
+          num_epochs=100, minibatch_size=64, print_cost=True):
+    """
+    Implements a three-layer ConvNet in Tensorflow:
+    CONV2D -> RELU -> MAXPOOL -> CONV2D -> RELU -> MAXPOOL -> FLATTEN -> FULLYCONNECTED
+
+    Arguments:
+    X_train -- training set, of shape (None, 64, 64, 3)
+    Y_train -- test set, of shape (None, n_y = 6)
+    X_test -- training set, of shape (None, 64, 64, 3)
+    Y_test -- test set, of shape (None, n_y = 6)
+    learning_rate -- learning rate of the optimization
+    num_epochs -- number of epochs of the optimization loop
+    minibatch_size -- size of a minibatch
+    print_cost -- True to print the cost every 100 epochs
+
+    Returns:
+    train_accuracy -- real number, accuracy on the train set (X_train)
+    test_accuracy -- real number, testing accuracy on the test set (X_test)
+    parameters -- parameters learnt by the model. They can then be used to predict.
+    """
+
+    ops.reset_default_graph()  # to be able to rerun the model without overwriting tf variables
+    tf.set_random_seed(1)  # to keep results consistent (tensorflow seed)
+    seed = 3  # to keep results consistent (numpy seed)
+    (m, n_H0, n_W0, n_C0) = X_train.shape
+    n_y = Y_train.shape[1]
+    costs = []  # To keep track of the cost
+
+    # Create Placeholders of the correct shape
+    X, Y = create_placeholders(n_H0, n_W0, n_C0, n_y)
+
+    # Initialize parameters
+    parameters = initialize_parameters()
+
+    # Forward propagation: Build the forward propagation in the tensorflow graph
+    Z3 = forward_propagation(X, parameters)
+
+    # Cost function: Add cost function to tensorflow graph
+    cost = compute_cost(Z3, Y)
+
+    # Backpropagation: Define the tensorflow optimizer. Use an AdamOptimizer that minimizes the cost.
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+    # Initialize all the variables globally
+    init = tf.global_variables_initializer()
+
+    # Start the session to compute the tensorflow graph
+    with tf.Session() as sess:
+
+        # Run the initialization
+        sess.run(init)
+
+        # Do the training loop
+        for epoch in range(num_epochs):
+
+            minibatch_cost = 0.
+            num_minibatches = int(m / minibatch_size)  # number of minibatches of size minibatch_size in the train set
+            seed = seed + 1
+            minibatches = random_mini_batches(X_train, Y_train, minibatch_size, seed)
+
+            for minibatch in minibatches:
+                # Select a minibatch
+                (minibatch_X, minibatch_Y) = minibatch
+                # IMPORTANT: The line that runs the graph on a minibatch.
+                # Run the session to execute the optimizer and the cost, the feedict should contain a minibatch for (X,Y).
+                _, temp_cost = sess.run([optimizer, cost], feed_dict={X: minibatch_X, Y: minibatch_Y})
+
+                minibatch_cost += temp_cost / num_minibatches
+
+            # Print the cost every epoch
+            if print_cost == True and epoch % 5 == 0:
+                print("Cost after epoch %i: %f" % (epoch, minibatch_cost))
+            if print_cost == True and epoch % 1 == 0:
+                costs.append(minibatch_cost)
+
+        # plot the cost
+        plt.plot(np.squeeze(costs))
+        plt.ylabel('cost')
+        plt.xlabel('iterations (per tens)')
+        plt.title("Learning rate =" + str(learning_rate))
+        plt.show()
+
+        # Calculate the correct predictions
+        predict_op = tf.argmax(Z3, 1)
+        correct_prediction = tf.equal(predict_op, tf.argmax(Y, 1))
+
+        # Calculate accuracy on the test set
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+        print(accuracy)
+        train_accuracy = accuracy.eval({X: X_train, Y: Y_train})
+        test_accuracy = accuracy.eval({X: X_test, Y: Y_test})
+        print("Train Accuracy:", train_accuracy)
+        print("Test Accuracy:", test_accuracy)
+
+        return train_accuracy, test_accuracy, parameters
+
+
 if __name__ == '__main__':
     np.random.seed(1)
 
@@ -100,6 +215,7 @@ if __name__ == '__main__':
     plt.figure()
     plt.imshow(X_train_orig[index])
     plt.show()
+    plt.close()
     print("y = " + str(np.squeeze(Y_train_orig[:, index])))
 
     X_train = X_train_orig / 255.
@@ -140,4 +256,23 @@ if __name__ == '__main__':
         a = sess.run(Z3, {X: np.random.randn(2, 64, 64, 3), Y: np.random.randn(2, 6)})
         print("Z3 = " + str(a))
 
-    a = 1
+    # 1.4 - Compute cost
+    tf.reset_default_graph()
+    with tf.Session() as sess:
+        np.random.seed(1)
+        X, Y = create_placeholders(64, 64, 3, 6)
+        parameters = initialize_parameters()
+        Z3 = forward_propagation(X, parameters)
+        cost = compute_cost(Z3, Y)  # if get the cost = 4.6648693 is OK
+        init = tf.global_variables_initializer()
+        sess.run(init)
+        a = sess.run(cost, {X: np.random.randn(4, 64, 64, 3), Y: np.random.randn(4, 6)})
+        print("cost = " + str(a))
+
+    # 1.5 Model
+    _, _, parameters = model(X_train, Y_train, X_test, Y_test, learning_rate=0.003, num_epochs=100)
+    fname = "images/thumbs_up.jpg"
+    image = np.array(ndimage.imread(fname, flatten=False))
+    my_image = scipy.misc.imresize(image, size=(64, 64))
+    plt.imshow(my_image)
+    plt.show()
