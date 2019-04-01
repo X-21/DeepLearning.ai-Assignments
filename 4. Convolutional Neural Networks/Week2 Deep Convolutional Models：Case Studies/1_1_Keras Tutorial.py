8559d8055c62dbf7fccb45d176186975e7f1908c
x-21 <edison-404@outlook.com>
2019-04-01 10:04:52 +0800

    finish c4w2 1_1

--- 4. Convolutional Neural Networks/Week2 Deep Convolutional Models：Case Studies/1_1_Keras Tutorial.py
+++ 4. Convolutional Neural Networks/Week2 Deep Convolutional Models：Case Studies/1_1_Keras Tutorial.py
@@ -1,22 +1,10 @@
-import numpy as np
-from keras import layers
 from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
 from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
 from keras.models import Model
-from keras.preprocessing import image
-from keras.utils import layer_utils
-from keras.utils.data_utils import get_file
-from keras.applications.imagenet_utils import preprocess_input
-# import pydot
-# from IPython.display import SVG
-from keras.utils.vis_utils import model_to_dot
-from keras.utils import plot_model
+
 from kt_utils import *
 
 import keras.backend as K
-
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import imshow
 
 import keras
 
@@ -96,4 +84,4 @@ if __name__ == '__main__':
     print("Loss = " + str(preds[0]))
     print("Test Accuracy = " + str(preds[1]))
 
-    a = 1
+    happyModel.summary()
